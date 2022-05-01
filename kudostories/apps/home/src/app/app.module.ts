c300651e11d1e400dce0d8d388d5cb0228d43479
flavioneubauer <flavio.neubauer@gmail.com>
2022-05-01 02:37:15 -0300

    versão inicial do monorepo

---
+++ kudostories/apps/home/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { MatSliderModule } from '@angular/material/slider';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { NxWelcomeComponent } from './nx-welcome.component';
+
+@NgModule({
+  declarations: [AppComponent, NxWelcomeComponent],
+  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, MatSliderModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
